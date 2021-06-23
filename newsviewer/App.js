8d74e2697ca0d9a025fbf4b5ba1964e7970edcd2
kimho <rlaghekd777@naver.com>
2021-06-23 20:42:19 +0900

    Newsviewer

---
+++ newsviewer/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import axios from "axios";
+import NewsList from "./component/NewsList";
+function App() {
+
+  return (
+    <div>
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
